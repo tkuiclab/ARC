c94dcc477f2cc9f6ab7d53141b2cc62a9d098103
Chen, Kung-Han <kbehouse@gmail.com>
2017-06-27 22:08:55 +0800

    update t2o.py with self.arm

--- strategy/scripts/t2o.py
+++ strategy/scripts/t2o.py
@@ -16,15 +16,13 @@ import tf
 
 import actionlib
 from std_msgs.msg import String, Float64
-from robotis_controller_msgs.msg import StatusMsg
-from manipulator_h_base_module_msgs.msg import IK_Cmd
-from manipulator_h_base_module_msgs.srv import GetKinematicsPose, GetKinematicsPoseResponse
 
 import obj_pose.msg
 import geometry_msgs.msg
 from geometry_msgs.msg import Twist
 import json
 
+import arm_task_rel
 
 #[0.3, 0, 0.3, -60, 0, 0, 0];
 _POS = (.2, 0, .3)  # x, y, z
@@ -38,232 +36,26 @@ cam2tool_y = -0.095  #cam axis
 cam2tool_z = 0.25 + 0.035
 
 
-class ArmTask:
+class T2O:
     """Running arm task class."""
 
     def __init__(self):
         """Inital object."""
-        self.__set_pubSub()
+        #self.__set_pubSub()
+        self.Arm 			= arm_task_rel.ArmTask()
+
         rospy.on_shutdown(self.stop_task)
-        #self.__set_mode_pub.publish('set')
-        self.__is_busy = False
+        self.Arm.busy = False
         self.__obj_pose_client = actionlib.SimpleActionClient("/obj_pose", obj_pose.msg.ObjectPoseAction)
-        #rospy.loginfo('Wait /obj_pose action...')
-        #self.__obj_pose_client.wait_for_server()
-
-    def __set_pubSub(self):
-        self.__set_mode_pub = rospy.Publisher(
-            '/robotis/base/set_mode_msg',
-            String,
-            latch=False,
-            queue_size=1
-        )
-
-        self.__ptp_pub = rospy.Publisher(
-            '/robotis/base/JointP2P_msg',
-            IK_Cmd,
-            latch=True,
-            queue_size=1
-        )
-
-        self.__cmd_pub = rospy.Publisher(
-            '/robotis/base/TaskP2P_msg',
-            IK_Cmd,
-            latch=True,
-            queue_size=1
-        )
-
-        self.__status_sub = rospy.Subscriber(
-            '/robotis/status',
-            StatusMsg,
-            self.__status_callback,
-            queue_size=10
-        )
-
         
-
-    def __status_callback(self, msg):
-        rospy.loginfo('msg.status_msg= '+ msg.status_msg)
-        if 'IK Failed' in msg.status_msg:
-            rospy.logwarn('ik fail')
-            self.stop_task()
-
-        elif 'End Trajectory' in msg.status_msg:
-            rospy.loginfo('In End Trajectory')
-            self.__is_busy = False
-
-    def pub_ikCmd(self, mode='line', pos=_POS, euler=_ORI):
-        """Publish ik cmd msg to manager node."""
-        while self.__is_busy:
-            rospy.sleep(.1)
-        cmd = []
-
-        for p in pos:
-            cmd.append(p)
-        for e in euler:
-            cmd.append(e)
-
-        rospy.loginfo('Sent:{}'.format(cmd))
-        self.__is_busy = True  #!!!!!!!!!
-
-        if 'line' == mode:
-            self.__cmd_pub.publish(cmd)
-        elif 'ptp' == mode:
-            self.__ptp_pub.publish(cmd)
-
-    def set_mode(self):    
-        self.__set_mode_pub.publish('set')
-
+        
     def stop_task(self):
         """Stop task running."""
-        self.__is_busy = False
-        self.__set_mode_pub.publish('')
-
-    def get_fb(self):
-        rospy.wait_for_service('/robotis/base/get_kinematics_pose')
-        try:
-            get_endpos = rospy.ServiceProxy(
-                '/robotis/base/get_kinematics_pose',
-                GetKinematicsPose
-            )
-            res = get_endpos('arm')
-            return res
-        except rospy.ServiceException, e:
-            print "Service call failed: %s" % e
-
-    def quaternion2euler(self, ori):
-        quaternion = (
-            ori.x,
-            ori.y,
-            ori.z,
-            ori.w
-        )
-        euler = tf.transformations.euler_from_quaternion(quaternion)
-        roll = euler[0]
-        pitch = euler[1]
-        yaw = euler[2]
-
-        # if degrees( abs(roll) )  > 90 and  degrees( abs(yaw) )  > 90: 
-        #     pitch = -pi/2 -(pi/2 - abs(pitch) )
-        #     roll = 0
-        #     yaw = 0
-
-        return (pitch, roll, yaw)
-
-    def euler2rotation(self, euler):  # jmp_rot
-        Cx = cos(euler[1])
-        Sx = sin(euler[1])
-        Cy = cos(euler[0])
-        Sy = sin(euler[0])
-        Cz = cos(euler[2])
-        Sz = sin(euler[2])
-
-        return [
-            [Cz * Sy + Sz * Sx * Cy,   Cz * Cy - Sz * Sx * Sy,  -Sz * Cx],
-            [Sz * Sy - Cz * Sx * Cy,   Sz * Cy + Cz * Sx * Sy,   Cz * Cx],
-            [               Cx * Cy,                 -Cx * Sy,        Sx]
-        ]
-
-    def rotation2vector(self, rot):
-        vec_n = [rot[0][0], rot[1][0], rot[2][0]]
-        vec_s = [rot[0][1], rot[1][1], rot[2][1]]
-        vec_a = [rot[0][2], rot[1][2], rot[2][2]]
-        return vec_n, vec_s, vec_a
-
-    def relative_control_rotate(self, mode='ptp', x=0, y=0, z=0, pitch=0, roll=0, yaw=0):
-        """Get euler angle and run task."""
-        while self.__is_busy:
-            rospy.loginfo('busy..')
-            rospy.sleep(.1)
-
-        #fb = task.get_fb()
-        fb = self.get_fb()
+        self.Arm.stop_task()
         
-
-        pos = fb.group_pose.position
-        ori = fb.group_pose.orientation
-        euler = self.quaternion2euler(ori)
-
-    
-
-        #print 'ori_pos = ' + str(pos)
-        #print 'ori_euler = ' + str(euler)
-        #print 'i_pitch = ' + str(pitch)
-
-        self.pub_ikCmd(
-            mode,
-            (pos.x + x, pos.y + y, pos.z + z),
-            (
-                degrees(euler[1]) + pitch,
-                degrees(euler[0]) + roll,
-                degrees(euler[2]) + yaw
-            )
-        )
-
-        while self.__is_busy:
-           rospy.sleep(.1)
-
-    def relative_control(self, mode='ptp', n=0, s=0, a=0):
-        """Get euler angle and run task."""
-        while self.__is_busy:
-            rospy.sleep(.1)
-
-        #fb = task.get_fb()  jmp_rel
-        fb    = self.get_fb()
-        pos   = fb.group_pose.position
-        ori   = fb.group_pose.orientation
-        euler = self.quaternion2euler(ori)
-        rot   = self.euler2rotation(euler)
-        vec_n, vec_s, vec_a = self.rotation2vector(rot)
-
-        move = [0, 0, 0]
-        if n != 0:
-            move += multiply(vec_n, n)
-        if s != 0:
-            move += multiply(vec_s, s)
-        if a != 0:
-            move += multiply(vec_a, a)
-
-        self.pub_ikCmd(
-            mode,
-            (pos.x + move[1], pos.y + move[0], pos.z + move[2]),
-            (
-                degrees(euler[1]), #pitch
-                degrees(euler[0]), #roll
-                degrees(euler[2])  #yaw
-            )
-        )
-
-        while self.__is_busy:
-            rospy.sleep(.1)
-    
-    def relative_xyz_base(self, mode='ptp', x=0, y=0, z=0):
-        """Get euler angle and run task."""
-        while self.__is_busy:
-            rospy.sleep(.1)
-
-        fb = self.get_fb()
-        pos = fb.group_pose.position
-        ori = fb.group_pose.orientation
-        euler = self.quaternion2euler(ori)
-
-        self.pub_ikCmd(
-            mode,
-            (pos.x + x, pos.y + y, pos.z + z),
-            (
-                degrees(euler[0]),
-                degrees(euler[1]),
-                degrees(euler[2])
-            )
-        )
-
-        while self.__is_busy:
-            rospy.sleep(.1)
-
-
     #request object pose
     def obj_pose_request(self, obj):
-        while self.__is_busy:
+        while self.Arm.busy:
             rospy.sleep(.1)
         rospy.loginfo('obj_pose_request()')
 
@@ -276,9 +68,7 @@ class ArmTask:
         self.__obj_pose_client.wait_for_result()
 
     def desk_photo_pose(self):
-        self.pub_ikCmd('ptp', (0.30, 0.0 , 0.2), (-180, 0, 0) )
-        #self.pub_ikCmd('ptp', (0.30, 0.0 , 0.3), (-90, 0, 0) )
-
+        self.Arm.pub_ikCmd('ptp', (0.30, 0.0 , 0.2), (-180, 0, 0) )
 
     def obj_pose_feedback_cb(self,fb):
         rospy.loginfo("In obj_pose_feedback_cb")
@@ -321,7 +111,7 @@ class ArmTask:
         self.relative_control_rotate( pitch = pitch)
    
         
-        while self.__is_busy:
+        while self.Arm.busy:
             rospy.sleep(.1)
 
         
@@ -341,61 +131,30 @@ class ArmTask:
               ', base_z='+str(-move_cam_z))
         
 
-        self.relative_control(n = move_cam_y , s= -move_cam_x, a = move_cam_z)
-      
+        self.Arm.elative_move_nsa(n = move_cam_y , s= -move_cam_x, a = move_cam_z)
 
 
 if __name__ == '__main__':
 
     rospy.init_node('t2o', anonymous=True)
 
+    task = T2O()
+    #rospy.sleep(0.5)
+    #task.desk_photo_pose()
 
-    task = ArmTask()
-    rospy.sleep(0.5)
-    task.set_mode()
-    rospy.sleep(0.2)
+    #self.Arm.pub_ikCmd('ptp', (0.30, 0.0 , 0.2), (-180, 0, 0) )
 
+    #task.Arm.pub_ikCmd('ptp', (0.40, 0.0 , 0.2), (-180, 0, 0) )
 
-    task.desk_photo_pose()
+    # task.Arm.relative_move_nsa(n =  0.05) # cam_y
+    # task.Arm.relative_move_nsa(s = -0.05) # cam_x
+    # task.Arm.relative_move_nsa(a =  0.05)
 
-    #task.pub_ikCmd('ptp', (0.30, 0.0 , 0.2), (-89, 0, 0) )
-        
-    #task.relative_control(n=.05)
-    #task.relative_control(n=.05)  # -cam_y
-    #task.relative_control(s=-.15)  # cam_x
-    #task.relative_control(a=.05)  #cam_z   jmp_str
-
-    task.pub_ikCmd('ptp', (0.30, 0.0 , 0.2), (-180, 0, 30) )
-
-    #pitch , cam_roll 
-    #task.relative_control_rotate(pitch=0, roll=0, yaw=0)   
- 
-    # X, Z, Y  
-    #roll, cam_yaw
-
-    #task.relative_control_rotate(pitch=0, roll=15, yaw=0)   
-
-    #roll, cam_pitch
-    #task.relative_control_rotate(pitch=0, roll=0, yaw=5)   
-
-
-
-    
-    #stow photo pose  
-    #task.pub_ikCmd('ptp', (0.30, 0 , 0.2), (-60, 90, -90, 0) )
-
-    #rospy.loginfo('strategy ready')
-    #task.obj_pose_request("dvdRobots")
-    
+    #task.Arm.relative_rot_nsa(s =  30)     # pitch -> cam_x
+    #task.Arm.relative_rot_nsa(a =  10)     # cam_z
+    #task.Arm.relative_rot_nsa(n = 10)     # cam_y
 
     r = rospy.Rate(10)
     while not rospy.is_shutdown():
         #print 'ok...'
         r.sleep()
-
-    #task.relative_control(n=.05)  # -cam_y
-    #task.relative_control(s=.05)  # cam_x
-    #task.relative_control(a=.05)  #cam_z   jmp_stra
-
-    #task.desk_photo_pose()
-    #task.pub_ikCmd('ptp')
